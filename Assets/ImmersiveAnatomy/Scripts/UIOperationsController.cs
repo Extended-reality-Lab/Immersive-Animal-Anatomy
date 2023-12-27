a4f047b35c83569c8c4b5a5e24ab7e25a18c26fd
Quinn Hoffman <31490850+megajolt@users.noreply.github.com>
2023-12-27 14:31:42 -0800

    Sliders added, arrays now filled dynamically

--- Assets/ImmersiveAnatomy/Scripts/UIOperationsController.cs
+++ Assets/ImmersiveAnatomy/Scripts/UIOperationsController.cs
@@ -6,60 +6,92 @@ using UnityEngine.UI;
 
 public class UIOperationsController : MonoBehaviour
 {
-    public GameObject arrayHolder;
-
-    private ArrayHolder arrayHolderScript;
+    
     public GameObject[] SkeletonArray;
     public GameObject[] OrganArray;
     public GameObject[] SkinArray;
+
+    private bool isModel = true;
+
+    private string currentScene;
     
-    public void Start(){
-        arrayHolderScript=arrayHolder.GetComponent<ArrayHolder>();
-        SkeletonArray=arrayHolderScript.SkeletonArray;
-        OrganArray=arrayHolderScript.OrganArray;
-        SkinArray=arrayHolderScript.SkinArray;
+    public void FillArrays(){
+        if(GameObject.FindWithTag("Model")!=null){
+            SkeletonArray.Initialize();
+            OrganArray.Initialize();
+            SkinArray.Initialize();
+            // Find all children of the Skeleton object
+            SkeletonArray = GameObject.Find("Skeleton").GetComponentsInChildren<GameObject>();
+            // Find all children of the Organ object
+            OrganArray = GameObject.Find("Organ").GetComponentsInChildren<GameObject>();
+            // Find all children of the Skin object
+            SkinArray = GameObject.Find("Skin").GetComponentsInChildren<GameObject>();
+            }
+            else{
+                isModel = false;
+            }
     }
     public void SkeletonSlider(float sliderVal)
     {
-        for (int i = 0; i < SkeletonArray.Length; i++)
-        {
-            Renderer renderer = SkeletonArray[i].GetComponent<Renderer>();
-            Material material = renderer.material;
-            Color color = material.color;
-            color.a = sliderVal; // Set the alpha value
-            material.color = color;
+        if(isModel){
+            for (int i = 0; i < SkeletonArray.Length; i++)
+            {
+                Renderer renderer = SkeletonArray[i].GetComponent<Renderer>();
+                Material material = renderer.material;
+                Color color = material.color;
+                color.a = sliderVal; // Set the alpha value
+                material.color = color;
+            }
+        }
+        else{
+            Debug.Log("No Model Found");
         }
     }
     public void SkinSlider(float sliderVal)
     {
-        for (int i = 0; i < SkinArray.Length; i++)
-        {
-            Renderer renderer = SkinArray[i].GetComponent<Renderer>();
-            Material material = renderer.material;
-            Color color = material.color;
-            color.a = sliderVal; // Set the alpha value
-            material.color = color;
+        if(isModel){
+            for (int i = 0; i < SkinArray.Length; i++)
+            {
+                Renderer renderer = SkinArray[i].GetComponent<Renderer>();
+                Material material = renderer.material;
+                Color color = material.color;
+                color.a = sliderVal; // Set the alpha value
+                material.color = color;
+            }
         }
+        else{
+            Debug.Log("No Model Found");
+        }
+        
     }
      public void OrganSlider(float sliderVal)
     {
-        for (int i = 0; i < OrganArray.Length; i++)
-        {
-            Renderer renderer = OrganArray[i].GetComponent<Renderer>();
-            Material material = renderer.material;
-            Color color = material.color;
-            color.a = sliderVal; // Set the alpha value
-            material.color = color;
+        if(isModel){
+            for (int i = 0; i < OrganArray.Length; i++)
+            {
+                Renderer renderer = OrganArray[i].GetComponent<Renderer>();
+                Material material = renderer.material;
+                Color color = material.color;
+                color.a = sliderVal; // Set the alpha value
+                material.color = color;
+            }
+        }
+        else{
+            Debug.Log("No Model Found");
         }
     }
 
     public void AllSlider(float sliderVal){
+        if(isModel){
         SkeletonSlider(sliderVal);
         SkinSlider(sliderVal);
         OrganSlider(sliderVal);
+        }
+        else{
+            Debug.Log("No Model Found");
+        }
     }
     public void modelButton(){
-
     }
     public void roomButton(){
 
@@ -67,4 +99,16 @@ public class UIOperationsController : MonoBehaviour
     public void gameButton(){
 
     }
+
+    public void Start(){
+            currentScene = SceneManager.GetActiveScene().name;
+            
+        }   
+    public void Update(){
+        if(SceneManager.GetActiveScene().name != currentScene/*&&SceneManager.GetActiveScene().name != "Lobby"*/){
+            FillArrays();
+            currentScene = SceneManager.GetActiveScene().name;
+        }
+    }
+    
 }
