cf8db99758ae31c777128ba77b615a3617824ad7
Quinn Hoffman <31490850+megajolt@users.noreply.github.com>
2023-12-28 13:25:28 -0800

    Set up raycast for UI stuff

--- Assets/ImmersiveAnatomy/Scripts/uiRaycast.cs
+++ Assets/ImmersiveAnatomy/Scripts/uiRaycast.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIRaycast : MonoBehaviour
 {
+    Ray ray;
+    public LayerMask layerMask;
+    int maxDistance = 100;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ray= new Ray(transform.position, transform.forward);
+        CheckCollisions();
+    }
+
+    void CheckCollisions()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, maxDistance, layerMask))
+        {
+            Debug.Log(hit.collider.gameObject.name+ " is selected");
+           
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+      
     }
 }
