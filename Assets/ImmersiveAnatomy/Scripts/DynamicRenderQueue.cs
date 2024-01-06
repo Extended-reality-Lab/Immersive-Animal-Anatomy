499752473e61edab1a35fcb1dd52a0f8e2dbdd4f
Quinn Hoffman <qhh746@gmail.com>
2024-01-05 17:14:46 -0800

    Update DynamicRenderQueue.cs

--- Assets/ImmersiveAnatomy/Scripts/DynamicRenderQueue.cs
+++ Assets/ImmersiveAnatomy/Scripts/DynamicRenderQueue.cs
@@ -7,29 +7,24 @@ public class DynamicRenderQueue : MonoBehaviour
 {
     private string currentScene;
     private int renderQueue = 3000;
-    private bool isModel = true;
+    private Vector3 cameraPosition;
     public List<GameObject> ModelArray;
     private MaterialPropertyBlock matBlock;
     public Camera Camera;
+
     // Start is called before the first frame update
     void Start()
     {
+        cameraPosition = Camera.transform.position;
+        Debug.Log("Camera Position: " + cameraPosition);
         currentScene = SceneManager.GetActiveScene().name;
-        if(GameObject.FindWithTag("Model")!=null){
-            isModel = true;
-        }
-        else{
-            isModel = false;
-        }
     }
-
-    
     void FixedUpdate()
     {
+        ;
         if(SceneManager.GetActiveScene().name != currentScene){
             currentScene = SceneManager.GetActiveScene().name;
             if(GameObject.FindWithTag("Model")!=null){
-                isModel = true;
                 // Find all children of the Skeleton object
                 Transform[] allSkeleChildren = GameObject.Find("Model Skeleton").GetComponentsInChildren<Transform>();
                 foreach (Transform child in allSkeleChildren)
@@ -59,26 +54,42 @@ public class DynamicRenderQueue : MonoBehaviour
                     }
                     
                 }
-            }
-            else{
-                isModel = false;
-            }
-        }
-        if(isModel){
-            for (int i = 0; i < ModelArray.Count; i++)
-            {
-                DistFromCamera(ModelArray[i]);
-                Renderer rend = ModelArray[i].GetComponent<Renderer>();
-                rend.GetPropertyBlock(matBlock);
-                matBlock.SetFloat("_RenderQueue", 3000-renderQueue);
-                rend.SetPropertyBlock(matBlock);
-                Debug.Log("object render queue:"+rend.material.renderQueue);
+                for (int i=0; i<ModelArray.Count; i++){
+                    Renderer rend = ModelArray[i].GetComponent<Renderer>();
+                    if(matBlock == null){
+                        matBlock = new MaterialPropertyBlock();
+                    }
+                    rend.GetPropertyBlock(matBlock);
+                    matBlock.SetFloat("_RenderQueue", renderQueue);
+                    matBlock.SetFloat("_RenderQueue", renderQueue-(Vector3.Distance(Camera.transform.position, ModelArray[i].transform.position)*10));
+                    rend.SetPropertyBlock(matBlock);
+                    Debug.Log("matBlock renderQueue: " + matBlock.GetFloat("_RenderQueue"));
+                }
             }
         }
+        RenderQueueUpdate();
+        cameraPosition = Camera.transform.position;
     }
 
-    void DistFromCamera(GameObject obj)
+    void RenderQueueUpdate()
     {
-        renderQueue = (int)Vector3.Distance(Camera.main.transform.position, obj.transform.position);
+        if (Camera.transform.position.x >= cameraPosition.x + 1 || Camera.transform.position.x <= cameraPosition.x - 1 || Camera.transform.position.y >= cameraPosition.y + 1 || Camera.transform.position.y <= cameraPosition.y - 1 || Camera.transform.position.z >= cameraPosition.z + 1 || Camera.transform.position.z <= cameraPosition.z - 1)
+        {
+            for (int i = 0; i < ModelArray.Count; i++)
+            {
+                Renderer rend = ModelArray[i].GetComponent<Renderer>();
+                if (matBlock == null)
+                {
+                    matBlock = new MaterialPropertyBlock();
+                }
+                rend.GetPropertyBlock(matBlock);
+                float distance = Vector3.Distance(Camera.transform.position, ModelArray[i].transform.position);
+                matBlock.SetFloat("_RenderQueue", renderQueue - (distance * 10));
+                rend.SetPropertyBlock(matBlock);
+                rend.material.renderQueue = (int)matBlock.GetFloat("_RenderQueue");
+                Debug.Log("Renderer renderQueue: " + rend.material.renderQueue + " bodyPart " + ModelArray[i].name + " bodyMat" + ModelArray[i].GetComponent<Renderer>().material.name);
+            }
+        }
     }
+    
 }
