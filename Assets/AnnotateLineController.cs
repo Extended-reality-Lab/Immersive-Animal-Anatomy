44ce105a6d354f09ea4bfe5861b0727a6176bcfc
Klearo <Klearouwu@gmail.com>
2024-02-09 12:33:02 -0800

    Annotation work

--- Assets/AnnotateLineController.cs
+++ Assets/AnnotateLineController.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class AnnotateLineController : MonoBehaviour
 {
 
     public InputActionReference drawButton;
 
-    public bool annotating = true;
+    public bool annotating = false;
+    public bool holderPresent = false;
 
     private LineRenderer line;
     private GameObject lineObject;
@@ -20,11 +22,14 @@ public class AnnotateLineController : MonoBehaviour
 
     [SerializeField]
     GameObject controllerPosition;
+    GameObject AnnotationHolder;
 
     [SerializeField]
     private float minDistance = .1f;
 
     private void Start(){
+
+        SceneManager.activeSceneChanged += ChangedActiveScene;
 
         drawButton.action.started += startedDraw;
         drawButton.action.canceled += endedDraw;
@@ -63,14 +68,34 @@ public class AnnotateLineController : MonoBehaviour
 
     void startedDraw(InputAction.CallbackContext ctx){
         //initialize a new line prefab, and set the annotating bool to true
-        lineObject = Instantiate(linePrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
-        line = lineObject.GetComponent<LineRenderer>();
-        annotating = true;
+        if (holderPresent == true) {
+            lineObject = Instantiate(linePrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
+            lineObject.transform.parent = AnnotationHolder.transform;
+            line = lineObject.GetComponent<LineRenderer>();
+            annotating = true;
+        }
     }
 
     void endedDraw(InputAction.CallbackContext ctx){
         //set the annotating bool to false
         annotating = false;
+    }
+
+    private void ChangedActiveScene(Scene current, Scene next)
+    {
+
+        //check to see if there is an animal in this scene
+        AnnotationHolder = GameObject.Find("/AnnotationHolder");
+
+        if (AnnotationHolder != null)
+        {
+            holderPresent = true;
+        }
+        else
+        {
+            holderPresent = false;
+        }
+
     }
 
 
