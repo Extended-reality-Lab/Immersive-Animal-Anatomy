0a666d173cd25aaa7c8783a80171b804d2a8cfd3
Quinn Hoffman <31490850+megajolt@users.noreply.github.com>
2024-02-08 17:42:11 -0800

    Initial code for labels and headtracking

---
+++ Assets/ImmersiveAnatomy/Scripts/LabelOperation.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LabelOperation : MonoBehaviour
+{
+    public GameObject Label;
+    public Transform playerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        setLabel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        showLabel();
+    }
+    void setLabel()
+    {
+        Label.GetComponentInChildren<Text>().text = transform.parent.name;
+    }
+    void showLabel()
+    {
+        if(Label.activeInHierarchy == true){
+            Label.SetActive(false);
+            trackLabel();
+        }
+        if(Label.activeInHierarchy == false){
+            Label.SetActive(true);
+        }
+    }
+    void trackLabel()
+    {
+        Label.transform.LookAt(playerCamera);
+    }
+}
