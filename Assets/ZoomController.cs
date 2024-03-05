ef155fe3edb0252824bcfd0ee7abb48618825c4c
Klearo <Klearouwu@gmail.com>
2024-03-04 23:26:28 -0800

    Added zoom toggle

--- Assets/ZoomController.cs
+++ Assets/ZoomController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ZoomController : MonoBehaviour
 {
@@ -9,10 +10,16 @@ public class ZoomController : MonoBehaviour
     public Material myMaterial;
     public GameObject magGlass;
     public Camera zoomCam;
+    public InputActionReference zoomToggle;
+    public bool zoomed;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        //create the input reference
+        zoomToggle.action.started += zoomToggled;
+
         //instantiate a new material and texture renderer
         rt = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
         rt.Create();
@@ -24,6 +31,26 @@ public class ZoomController : MonoBehaviour
         //assign it
         magGlass.GetComponent<MeshRenderer>().material = myMaterial;
 
+        zoomed = false;
+        magGlass.SetActive(false);
+
+    }
+
+    void zoomToggled(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Pluh!");
+        if (zoomed == false)
+        {
+            //enable it
+            magGlass.SetActive(true);
+        }
+        else
+        {
+            //disable it
+            magGlass.SetActive(false);
+        }
+
+        zoomed = !zoomed;
     }
 
 
