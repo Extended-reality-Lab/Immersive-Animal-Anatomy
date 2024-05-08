f74d3797640b062d51916e384ab1daca9b256ce5
Quinn Hoffman <qhh746@gmail.com>
2024-05-08 13:36:39 -0700

    Scripts Added

---
+++ Assets/ImmersiveAnatomy/Scripts/LabelTracking.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class LabelTracking : MonoBehaviour
+{
+    public Transform playerCamera;
+    public bool uiShowing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    void Update()
+    {
+        transform.LookAt(playerCamera, Vector3.up);
+        transform.Rotate(0, 180, 0);
+    }
+
+
+}
